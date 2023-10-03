7a47a0e3da752e9f93c836dd35c8316623cd9412
DuBoris Smith <duboris.smith@hotmail.com>
2023-10-02 19:09:49 -0500

    Added extra cart components

--- Finale-Capstone/E-comCapstone/src/App.jsx
+++ Finale-Capstone/E-comCapstone/src/App.jsx
@@ -19,7 +19,7 @@ import CreateCart from './components/carts/CreateCart';
 // import EditCart from './Test.js/testEdit';
 
 function App() {
-  const { productItems } = data;
+  // const { productItems } = data;
   const [cartItems, setCartItems] = useState([])
 
   const handleAddProduct = (product) =>{
@@ -32,14 +32,32 @@ function App() {
       setCartItems([...cartItems, {...product, quantity: 1}]);
     }
   }
- 
 
+  const handleRemoveProduct = (product) =>{
+    const ProductExist = cartItems.find((item) => item.id === product.id);
+    if(ProductExist.quantity === 1){
+      setCartItems(cartItems.filter((item) => item.id !== product.id))
+    } else {
+      setCartItems(
+        cartItems.map((item) => 
+          item.id === product.id 
+          ? { ...ProductExist, quantity: ProductExist.quantity -1} 
+          : item
+        )
+      );
+    }
+  };
+ 
+  // productItems={productItems}
   return (
     <>
       <Navigation />
       <AuthProvider>
         <React.Fragment>
-          <Routes productItems={productItems} cartItems={cartItems} handleAddProduct={handleAddProduct}>
+          <Routes 
+            cartItems={cartItems} 
+            handleAddProduct={handleAddProduct}
+            handleRemoveProduct={handleRemoveProduct}>
             <Route
               path="/"
               element={
@@ -54,7 +72,7 @@ function App() {
               />
             <Route
               path="/products-list" exact
-              element={<AllProducts /> }/>
+              element={<AllProducts handleAddProduct={handleAddProduct}/> }/>
             <Route 
               path="/products/:productId" exact
               element={<SingleProduct/>}/>
--- Finale-Capstone/E-comCapstone/src/components/carts/CreateCart.jsx
+++ Finale-Capstone/E-comCapstone/src/components/carts/CreateCart.jsx
@@ -1,25 +1,40 @@
 import React from 'react'
 import "../styles/cart.css"
 
-const CreateCart = ({cartItems}) => {
+const CreateCart = ({cartItems, handleAddProduct, handleRemoveProduct}) => {
   return (
     <div className="cart-items">
         <div className="cart-items-header">
           <header className='cart-title'>
           Cart Items
           </header>
-          </div>
+        </div>
 
         {cartItems.length === 0 && (
             <div className="cart-items-empty"> No Items are added</div>    
         )}
             {cartItems.map(() => (
+              <>
                 <div key={item.id} className="cart-items-list">
                     <img 
                         src={item.image} 
                         alt={item.title} 
                         className="cart-items-image" />
                 </div>
+                <div className="cart-items-name">{item.name}</div>
+                <div className="cart-items-function">
+                  <button 
+                    className="cart-items-add" 
+                    onClick={() => handleAddProduct(item)}>
+                      +
+                    </button>
+                  <button 
+                    className="cart-items-remove"
+                    onClick={() => handleRemoveProduct(item)}>
+                      -
+                      </button>
+                </div>
+              </>
             ))}
     </div>
   )
